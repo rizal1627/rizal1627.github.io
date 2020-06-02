860d08b769fb3eeeebefd2ae91bd4624cb9f2868
rizal1627 <setioajirizal@gmail.com>
2020-06-02 21:16:23 +0700

    initial awal

---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, Test</title>
+</head>
+<body>
+    <h1>
+        Hello
+    </h1>
+    <?php 
+        $yes = 1;
+        $k = 1;
+        $hasil = $yes + $k;
+
+        echo $hasil;
+        if($hasil>=3){
+            echo'Yess';
+        }
+        else {
+            echo 'yahhh';
+        }
+    ?>
+</body>
+</html>
